bfa3944f25814097bed6997b6c26c9ec08722c9c
Josepinto <jose-pedro.pinto@capgemini.com>
2022-08-19 04:09:06 +0100

    bf

--- redes/main.tf
+++ redes/main.tf
@@ -48,12 +48,12 @@ resource "aws_route_table" "public" {
   }
 }
 
-resource "aws_route_table_association" "public" {
+/* resource "aws_route_table_association" "public" {
   for_each       = aws_subnet.public
   route_table_id = aws_route_table.public[each.key].id
   subnet_id      = each.value.id
 }
-
+ */
 
 ###################
 ###########
